e7ba14e9d14a7057bc30ab27b4cb914a47495b09
Vladimir <jaln.102406@gmail.com>
2023-05-30 06:05:48 +0300

    release 1.1.1

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.URL,
+    baseURL: 'http://82.148.30.227:8080/api',
     headers: {
         "Content-type": "application/json"
     }
